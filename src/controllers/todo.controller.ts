d436d23f317be32f026d9eb75d939ad969a83e6a
harrison <d.harrison.lima@gmail.com>
2024-07-14 12:45:58 -0300

    feat: add route update

--- src/controllers/todo.controller.ts
+++ src/controllers/todo.controller.ts
@@ -21,6 +21,33 @@ export const add = async (req: Request, res: Response) => {
   return res.json({ erro: "Dados não enviados" });
 };
 
-export const update = async () => {};
+export const update = async (req: Request, res: Response) => {
+  const id: string = req.params.id;
 
-export const remove = async () => {};
+  let todo = await Todo.findByPk(id);
+  if (todo) {
+    if (req.body.title) {
+      todo.title = req.body.title;
+    }
+
+    if (req.body.done) {
+      switch (req.body.done.toLowerCase()) {
+        case "true":
+        case "1":
+          todo.done = true;
+          break;
+        case "false":
+        case "0":
+          todo.done = false;
+          break;
+      }
+    }
+
+    await todo.save();
+    return res.json({ item: todo });
+  }
+
+  return res.json({ error: "Item não encontrado" });
+};
+
+export const remove = async (req: Response, res: Response) => {};
